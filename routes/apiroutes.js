227bdf34adf031d6325881322a5b31b55ddf8076
Najua Saad <najuasaad@gmail.com>
2021-04-29 09:30:29 -0500

    commented in apiroutes

---
+++ routes/apiroutes.js
@@ -0,0 +1,16 @@
+// require in router and model
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+// create new workout
+
+// update workout (continue)
+
+// (re)view workout
+
+// view total weight?
+
+// delete workout
+
+// export router
+module.exports = router;
