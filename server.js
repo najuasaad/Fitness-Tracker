efe65649b6ae7849d6ae83ca111551e68db191f4
Najua Saad <najuasaad@gmail.com>
2021-04-28 19:21:56 -0500

    boiler plate server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const databaseUrl = "fitness";
+const collections = ["daily-workouts"];
+const db = mongojs(databaseUrl, collections);
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
