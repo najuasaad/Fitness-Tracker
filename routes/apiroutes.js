2828a4fea7f4e4da4379c97ddf96a2ad1a54b611
Najua Saad <najuasaad@gmail.com>
2021-04-29 20:29:22 -0500

    new workout working, new exercise testing

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,23 +3,23 @@ const router = require('express').Router();
 const db = require("../models")
 
 // addExercise
-/* router.put("/api/workouts/:id", ({body}, res) => {
+router.put("/api/workouts/:id", ({body}, res) => {
   db.Exercise.create(body)
-    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } } , { new: true}))
+    .then(({_id}) => db.Workout.findOneAndUpdate({ day: Date.now }, { $push: { exercises: _id } } , { new: true}))
     .then(result => {
       res.json(result);
     })
     .catch(err => {
       res.json(err)
     })
-}) */
+})
 
 // getLastWorkout
 router.get("/api/workouts", (req, res) => {
   db.Workout.find({})
   .populate("Exercise")
   .then(dbWorkout => {
-    console.log(dbWorkout)
+    //console.log(dbWorkout)
     res.json(dbWorkout)
   })
   .catch(err => {
@@ -27,13 +27,19 @@ router.get("/api/workouts", (req, res) => {
   })
 })
 
-/* // createWorkout
-router.post("/api/workouts", (req, res) => {
-
+// createWorkout
+router.post("/api/workouts", ({body}, res) => {
+  db.Workout.create(body)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.json(err)
+  })
 })
 
 // getWorkoutsInRange
-router.get("/api/workouts/range", (req, res) => {
+/* router.get("/api/workouts/range", (req, res) => {
   db.Workout.find({})
   .populate("Exercise")
   .then(dbWorkout => {
