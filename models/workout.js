1fc9e33539ec463646f81fb3cbb9e1d547c2c88b
Najua Saad <najuasaad@gmail.com>
2021-04-30 13:00:48 -0500

    cleaned up model page

--- models/workout.js
+++ models/workout.js
@@ -37,8 +37,3 @@ const WorkoutSchema = new Schema({
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
-
-// exercises: [
-//   {type: Schema.Types.ObjectId,
-//   ref: 'Exercise'}
-// ]
